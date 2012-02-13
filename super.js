4dccafb8de5a74906f5f4a2f1321643ab7302d4e
Archangel_SDY <Archangel.SDY@gmail.com>
2012-02-14 03:36:58 +0800

    Reconstruct super.js.
    To be fixed:
    -Incorrect position for toolbar when img has margin.
    -When mouse leaves img and enters toolbar, toobar will be removed.
    -Cannot get the url of img when searching.

--- super.js
+++ super.js
@@ -1,229 +1,179 @@
-if(divInstant!=null){
-	//already running
-	for(var i=0;i<divInstant.length;i++){
-		var divImg=divInstant[i];
-		var img=divImg.childNodes[0];
+var iqdbInstant = function () {
+	var MIN_WEIGHT = 100;
+	var MIN_HEIGHT = 100;
 
+	var self = {};
+
+	self.enabled = true;
+
+	var wrapper = null; 
+
+	var showToolbar = function () {
+		if (wrapper === null) {
+			img = window.event ? window.event.srcElement : this;
+			var parent = img.parentNode;
+		    
+		    //Create wrapper div.
+		    var divWrapper = document.createElement("div");
+		    divWrapper.style.position = "relative";
+		    parent.appendChild(divWrapper);
+		    
+		    //Move image to the wrapper div.
+		    divWrapper.appendChild(parent.removeChild(img));
+		    
+		    //Add toolbar.
+		    toolbar = createToolbar(img);
+		    divWrapper.appendChild(toolbar);
+
+		    wrapper = divWrapper;
+		}
+	};
+
+	var hideToolbar = function () {
+		img = window.event ? window.event.srcElement : this;
+		
 		//remove the event listener
-		img.removeEventListener("mouseover",showToolbar,false);
-		img.removeEventListener("mouseout",hideToolbar,false);
+		img.removeEventListener("mouseover", showToolbar, false);
+		img.removeEventListener("mouseout", hideToolbar, false);
 
 		//remove the toolbar
-		divImg.parentNode.appendChild(divImg.removeChild(img));
-		divImg.parentNode.removeChild(divImg);
-	}
-	divInstant=null;
-	alert("imageInstant is unloaded.")
-}else{
-	//initialize
-	var divInstant=new Array();
-
-	var showToolbar=function(){
-		var toolbar=this.parentNode.lastChild;
-		while(toolbar.nodeType!=1){
-			toolbar=toolbar.previousSibling;
-		}
-		toolbar.style.opacity="0.8";
+		divWrapper = wrapper;
+		divWrapper.parentNode.appendChild(divWrapper.removeChild(img));
+		divWrapper.parentNode.removeChild(divWrapper);
+		wrapper = null;
 	};
 
-	var hideToolbar=function(){
-		var toolbar=this.parentNode.lastChild;
-		while(toolbar.nodeType!=1){
-			toolbar=toolbar.previousSibling;
+
+	self.initialize = function () {
+		var imgs = document.getElementsByTagName("img");
+		for (var i=0; i<imgs.length; i++) {
+			try {
+				//Only apply for images larger than the limit.
+				var img = new Image();
+				img.src = imgs[i].src;
+				if (img.width >= MIN_WEIGHT && img.height >= MIN_HEIGHT) {
+				    if (imgs[i].addEventListener) {
+				    	//alert(233);
+				    	imgs[i].addEventListener("mouseover", showToolbar, false);
+				    } else if (imgs[i].attachEvent) {
+				    	imgs[i].attachEvent("onmouseover", showToolbar);
+				    }
+				    if (imgs[i].addEventListener) {
+				    	imgs[i].addEventListener("mouseout", hideToolbar, false);
+				    } else if (imgs[i].attachEvent) {
+				    	imgs[i].attachEvent("onmouseout", hideToolbar);
+				    }
+		    	}
+			} catch (err) {
+				alert(err);
+			}
 		}
-		toolbar.style.opacity="0";
+		alert("imageInstant is ready.");
 	};
 
-	var imgs=document.getElementsByTagName("img");
-	for(var i=0;i<imgs.length;i++){
-		try{
-			//only apply for images larger than 100x100px
-			var img=new Image();
-			img.src=imgs[i].src;
-			if(img.width>=100 && img.height>=100){
-	        	createToolbar(imgs[i]);
-	    	}
-		}catch(err){
-			alert(err);
-		}
-	}
-	alert("imageInstant is ready.");
-}
+	var createToolbar = function (img) {
+	    var toolbar = document.createElement("ul");
+	    toolbar.style.margin = img.style.margin;
+	    toolbar.style.padding = "0px";
+	    toolbar.style.position = "absolute";
+	    toolbar.style.top = "0px";
+	    toolbar.style.left = "0px";
+	    toolbar.style.backgroundColor = "black";
+	    toolbar.style.listStyleType = "none";
 
-function createToolbar(img){
-	var parent=img.parentNode;
-	var imgSrc=img.src;
-	var id=divInstant.length;
-    
-    var divImg=document.createElement("div");
-    divImg.style.position="relative";
-    parent.appendChild(divImg);
-    
-    divImg.appendChild(parent.removeChild(img));
-    
-    var toolbar=document.createElement("ul");
-    toolbar.setAttribute("style","margin: 0px;padding: 0px;position: absolute;top: 0px;left: 0px;background-color: black;opacity: 0;list-style-type: none;");
 
-    toolbar.appendChild(createSearchEngine("iqdb","iqdbSearch('"+imgSrc+"');",id));
-    toolbar.appendChild(createSearchEngine("sauceNAO","sauceNAOSearch('"+imgSrc+"');",id));
-    toolbar.appendChild(createSearchEngine("Google","googleSearch('"+imgSrc+"');",id));
-    toolbar.appendChild(createSearchEngine("TinEye","tineyeSearch('"+imgSrc+"');",id));
-    toolbar.appendChild(createSearchEngine("Baidu","baiduSearch('"+imgSrc+"');",id));
-    divImg.appendChild(toolbar);
+	   	toolbar.onmouseover = function () {
+	   		toolbar.style.opacity = 0.8;
+	   	};
+	   	toolbar.onmouseout = function () {
+	   		toolbar.style.opacity = 0;
+	   	};
 
-    divInstant.push(divImg);
+	    for (engine in searchEngines) {
+			var engineLi = document.createElement("li");
+		    engineLi.style.display = "block";
+		    engineLi.style.margin = "0px";
+		    engineLi.style.padding = "0px";
 
-    if(img.addEventListener){
-    	img.addEventListener("mouseover",showToolbar,false);
-    }else if(img.attachEvent){
-    	img.attachEvent("onclick",showToolbar);
-    }
-    if(img.addEventListener){
-    	img.addEventListener("mouseout",hideToolbar,false);
-    }else if(img.attachEvent){
-    	img.attachEvent("onmouseout",hideToolbar);
-    }
-   	toolbar.setAttribute("onmouseover","this.style.opacity=0.8;");
-   	toolbar.setAttribute("onmouseout","this.style.opacity=0;");
-}
+		    var engineAnchor = document.createElement("a");
+		    engineAnchor.style.color = "white";
+		    engineAnchor.style.textDecoration = "none";
+		   	engineAnchor.style.margin = "5px";
+		    engineAnchor.href = "javascript:void(0); ";
+		    engineAnchor.innerHTML = engine;	//Set name.
+		    engineAnchor.onclick = function (href, searchEngine) {
+		    	return function () {
+		    		searchEngine(href);
+		    	};
+		    }(img.href, searchEngines[engine]);
+		    engineLi.appendChild(engineAnchor);
 
-function createSearchEngine(name,searchEngine,id){
-    var engineLi=document.createElement("li");
-    engineLi.setAttribute("style","display: block;margin: 0px;padding: 0px;");
-    var engineAnchor=document.createElement("a");
-    engineAnchor.setAttribute("style","color: white;text-decoration: none;margin: 5px;")   
-    engineAnchor.href="javascript:void(0);";
-    engineAnchor.setAttribute("onclick",searchEngine);
-    engineAnchor.innerHTML=name;
-    engineLi.appendChild(engineAnchor);
-    return engineLi;
-}
+		    toolbar.appendChild(engineLi);
+	    }
 
-function iqdbSearch(url){
-	var form=document.createElement("form");
-	form.action="http://iqdb.org/";
-	form.method="post";
-	form.enctype="multipart/form-data";
-	form.target="_blank";
+	    return toolbar;
+	};
 
-	var txtUrl=document.createElement("input");
-	txtUrl.type="hidden";
-	txtUrl.name="url";
-	txtUrl.value=url;
-	form.appendChild(txtUrl);
-	
-	document.body.appendChild(form);
-	form.submit();
-}
+	var searchEngines = [];
 
-function sauceNAOSearch(url){
-	var form=document.createElement("form");
-	form.action="http://saucenao.com/search.php";
-	form.method="post";
-	form.enctype="multipart/form-data";
-	form.target="_blank";
+	searchEngines['iqdb'] = function (url) {
+		var form = document.createElement("form");
+		form.action = "http://iqdb.org/";
+		form.method = "post";
+		form.enctype = "multipart/form-data";
+		form.target = "_blank";
 
-	var txtUrl=document.createElement("input");
-	txtUrl.type="hidden";
-	txtUrl.name="url";
-	txtUrl.value=url;
-	form.appendChild(txtUrl);
-	
-	var checkbox=document.createElement("input");
-	checkbox.type="hidden";
-	checkbox.name="urlify";
-	checkbox.value="true";
-	form.appendChild(checkbox);
+		var txtUrl = document.createElement("input");
+		txtUrl.type = "hidden";
+		txtUrl.name = "url";
+		txtUrl.value = url;
+		form.appendChild(txtUrl);
+		
+		document.body.appendChild(form);
+		form.submit();
+	};
 
-	document.body.appendChild(form);
-	form.submit();
-}
+	searchEngines['sauceNAO'] = function (url) {
+		var form = document.createElement("form");
+		form.action = "http://saucenao.com/search.php";
+		form.method = "post";
+		form.enctype = "multipart/form-data";
+		form.target = "_blank";
 
-function googleSearch(url){
-	var form=document.createElement("form");
-	form.action="http://www.google.com/searchbyimage";
-	form.method="GET";
-	form.enctype="multipart/form-data";
-	form.target="_blank";
+		var txtUrl = document.createElement("input");
+		txtUrl.type = "hidden";
+		txtUrl.name = "url";
+		txtUrl.value = url;
+		form.appendChild(txtUrl);
+		
+		var checkbox = document.createElement("input");
+		checkbox.type = "hidden";
+		checkbox.name = "urlify";
+		checkbox.value = "true";
+		form.appendChild(checkbox);
 
-	var txtUrl=document.createElement("input");
-	txtUrl.type="hidden";
-	txtUrl.name="image_url";
-	txtUrl.value=url;
-	form.appendChild(txtUrl);
+		document.body.appendChild(form);
+		form.submit();
+	};
 
-	document.body.appendChild(form);
-	form.submit();
-}
+	searchEngines['Google'] = function (url) {
+		var form = document.createElement("form");
+		form.action = "http://www.google.com/searchbyimage";
+		form.method = "GET";
+		form.enctype = "multipart/form-data";
+		form.target = "_blank";
 
-function tineyeSearch(url){
-	var form=document.createElement("form");
-	form.action="http://www.tineye.com/search";
-	form.method="POST";
-	form.target="_blank";
+		var txtUrl = document.createElement("input");
+		txtUrl.type = "hidden";
+		txtUrl.name = "image_url";
+		txtUrl.value = url;
+		form.appendChild(txtUrl);
 
-	var txtUrl=document.createElement("input");
-	txtUrl.type="hidden";
-	txtUrl.name="url";
-	txtUrl.value=url;
-	form.appendChild(txtUrl);
+		document.body.appendChild(form);
+		form.submit();	
+	};
 
-	document.body.appendChild(form);
-	form.submit();
-}
+	return self;
+}();
 
-function sakuraSearch(url){
-	var form=document.createElement("form");
-	form.action="http://www.tineye.com/search";
-	form.method="POST";
-	form.target="_blank";
-
-	var txtUrl=document.createElement("input");
-	txtUrl.type="hidden";
-	txtUrl.name="url";
-	txtUrl.value=url;
-	form.appendChild(txtUrl);
-
-	document.body.appendChild(form);
-	form.submit();
-}
-
-function baiduSearch(url){
-	var form=document.createElement("form");
-	form.action="http://stu.baidu.com/i?";
-	form.method="get";
-	form.target="_blank";
-
-	var txtUrl=document.createElement("input");
-	txtUrl.type="hidden";
-	txtUrl.name="objurl";
-	txtUrl.value=url;
-	form.appendChild(txtUrl);
-
-	var rt=document.createElement('input');
-	rt.type="hidden";
-	rt.name="rt";
-	rt.value='0';
-	form.appendChild(rt);
-
-	var rn=document.createElement('input');
-	rn.type="hidden";
-	rn.name="rn";
-	rn.value='10';
-	form.appendChild(rn);
-
-	var ct=document.createElement('input');
-	ct.type="hidden";
-	ct.name="ct";
-	ct.value='1';
-	form.appendChild(ct);
-
-	var tn=document.createElement('input');
-	tn.type="hidden";
-	tn.name="tn";
-	tn.value='baiduimage';
-	form.appendChild(tn);
-
-	document.body.appendChild(form);
-	form.submit();
-}
+iqdbInstant.initialize();
