166f6b7ad47f379959ce2420a02c3184d5955eeb
Archangel_SDY <Archangel.SDY@gmail.com>
2012-02-16 02:52:50 +0800

    Remove some unused code.

--- super.js
+++ super.js
@@ -1,3 +1,19 @@
+// Copyright (C) <2012>  <Archangel.SDY@gmail.com>
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 if (typeof imageInstant === 'undefined') {
 	var imageInstant = function () {
 		var MIN_WEIGHT = 100;
@@ -9,7 +25,6 @@ if (typeof imageInstant === 'undefined') {
 
 		self.enabled = true;
 
-		var wrapper = null;
 		var toolbarMap = [];
 
 		var getComputedStyle = function (element) {
@@ -64,8 +79,6 @@ if (typeof imageInstant === 'undefined') {
 			    	'toolbar': toolbar
 			    });
 			    divWrapper.appendChild(toolbar);
-
-			    wrapper = divWrapper;
 			} else {
 				toolbar.style.opacity = TOOLBAR_OPACITY;
 			}
@@ -80,7 +93,6 @@ if (typeof imageInstant === 'undefined') {
 				toolbar.style.opacity = 0;
 			}
 		};
-
 
 		self.initialize = function () {
 			if (self.enabled) {
